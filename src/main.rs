e6b50937a863ba024f0603ad89f142409fa7d4fd
wakhu shibero <wshibero@gmail.com>
2024-05-22 06:27:56 +0000

    prints

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
 fn main() {
     println!("Hello, world!");
-  println!("this is derrick shibero");
+    println!("this is derrick shibero");
+  println!("I am a rustician");
+  //variable definition
+  let x = 5+5;
+  // various prints
+  println!("is `x` equal to 10 or 1000: {}",x);
+  println!("january has {} days", 31);
+  println!("{0} this is {1} and {1} this is {0}", "Alice", "Bob");
+  println!("{subject} {verb} {object}",
+           object="the lazy dog",
+           verb="jumps over ",
+           subject="the quick brown fox");
 }
